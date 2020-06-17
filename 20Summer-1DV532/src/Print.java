b0e32c7fe414454e6d4bb5ac82d644cb1c13c07c
Axel <Axel@DESKTOP-TH3311B>
2020-06-17 21:39:09 +0200

    Print test

---
+++ 20Summer-1DV532/src/Print.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Print {
+
+	public static void main(String[] args) {
+		System.out.print("Enter something:");
+		Scanner scan = new Scanner(System.in);
+		String word = scan.nextLine();
+		System.out.print(word);
+		
+		
+		
+		
+	}
+
+
+}
