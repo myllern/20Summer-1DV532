b450800c2e9b328db476208011b8398e1d8393ff
Axel <Axel@DESKTOP-TH3311B>
2020-06-17 22:04:58 +0200

    prin

--- 20Summer-1DV532/src/Assignment1/Print.java
+++ 20Summer-1DV532/src/Assignment1/Print.java
@@ -1,5 +1,22 @@
 package Assignment1;
 
 public class Print {
+	public static void main(String[] args) {
+		String x, y, z;
+		x = "Write once, run everywhere!";
+		y = "Write" + "\n" + "once, " + "\n" + "run" + "\n" + "everywhere!";
+		z = "*******************************" + "\n" + "**Write once, run everywhere!**" + "\n" + "*******************************";
+		System.out.println("---------------Exercise 1-------------");
+		System.out.println();
+		System.out.println(x);
+		System.out.println();
+		System.out.println("--------------------------------------");
+		System.out.println();
+		System.out.println(y);
+		System.out.println();
+		System.out.println("--------------------------------------");
+		System.out.println();
+		System.out.println(z);
+	}
 
 }
