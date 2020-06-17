b95d328195e09d894cfc64e1a4b9f44a04cdd3c7
Axel <Axel@DESKTOP-TH3311B>
2020-06-17 23:33:29 +0200

    Num done

--- 20Summer-1DV532/src/Assignment1/Print.java
+++ 20Summer-1DV532/src/Assignment1/Print.java
@@ -5,7 +5,8 @@ public class Print {
 		String x, y, z;
 		x = "Write once, run everywhere!";
 		y = "Write" + "\n" + "once, " + "\n" + "run" + "\n" + "everywhere!";
-		z = "*******************************" + "\n" + "**Write once, run everywhere!**" + "\n" + "*******************************";
+		z = "*******************************" + "\n" + "**Write once, run everywhere!**" + "\n"
+				+ "*******************************";
 		System.out.println("---------------Exercise 1-------------");
 		System.out.println();
 		System.out.println(x);
