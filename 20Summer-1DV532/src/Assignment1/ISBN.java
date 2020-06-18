ff152fe36b4f9cce98ccda11142f099eb071be62
Axel <Axel@DESKTOP-TH3311B>
2020-06-18 08:19:13 +0200

    fix

--- 20Summer-1DV532/src/Assignment1/ISBN.java
+++ 20Summer-1DV532/src/Assignment1/ISBN.java
@@ -18,14 +18,14 @@ public class ISBN {
 				if (in.length() == 9) {
 
 					for (int i = 0; i < 9; i++) {
-						z = z + Integer.parseInt(String.valueOf(in.charAt(i)));
+						z = z + Integer.parseInt(String.valueOf(in.charAt(i)))*i;
 					}
 					int mod = z % 11;
 
 					if (mod == 10) {
-						System.out.print(in + "X");
+						System.out.print("The ISBN-10 number is: " + in + "X");
 					} else {
-						System.out.print(in + mod);
+						System.out.print("The ISBN-10 number is: " + in + mod);
 					}
 				} else {
 					System.out.print("Enter the first 9 digits of an ISBN ONLY as integer: ");
