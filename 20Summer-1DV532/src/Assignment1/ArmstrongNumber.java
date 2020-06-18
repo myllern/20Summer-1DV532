c4b703d245852389092c70cdeb3f001c65eda0ad
Axel <Axel@DESKTOP-TH3311B>
2020-06-18 13:37:12 +0200

    ArmStrong done

--- 20Summer-1DV532/src/Assignment1/ArmstrongNumber.java
+++ 20Summer-1DV532/src/Assignment1/ArmstrongNumber.java
@@ -1,6 +1,48 @@
 package Assignment1;
 
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class ArmstrongNumber {
-	
-//ködfhdf
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Boolean fin = true;
+		do {
+			try {
+				System.out.print("Enter the starting number of the range: ");
+				int inLow = scan.nextInt();
+				System.out.print("Enter the ending number of the range: ");
+				int inHigh = scan.nextInt();
+				int diff = inHigh - inLow;
+
+				for (int i = 0; i < diff; i++) {
+					int nrD = String.valueOf(inLow + i).length();
+					String nrS = String.valueOf(inLow + i);
+					int sum = 0;
+					int temp = 0;
+					for (int h = 0; h < nrD; h++) {
+						sum = sum + ((int) Math.pow(Integer.parseInt(String.valueOf(nrS.charAt(h))), nrD));
+						if (sum == Integer.parseInt(nrS)) {
+							
+							
+							if (temp != sum) { 
+								System.out.println(sum);
+								temp = sum;
+							}
+						}
+					}
+				}
+				fin = false;
+
+			} catch (InputMismatchException e) {
+				System.out.println();
+				System.out.print("NUMBERS only...");
+				System.out.println();
+				scan.nextLine();
+			}
+		} while (fin);
+	}
 }
